4a45dc531fa8dc28d6ddd22ffce873055cbaddde
Raphael Saldanha <rfsaldanha@gmail.com>
2023-10-03 15:21:36 +0200

    job prec 2020

---
+++ download_year_prec_2020.R
@@ -0,0 +1,71 @@
+# devtools::install_github("https://github.com/ErikKusch/KrigR")
+library(KrigR)
+library(tictoc)
+library(lubridate)
+
+years <- 2020
+months <- 1:12
+
+keyring_unlock("ecmwfr", password= Sys.getenv("era5_keyring"))
+
+API_User <- as.numeric(Sys.getenv("era5_API_User"))
+API_Key <- Sys.getenv("era5_API_Key")
+
+Dir.Data <- "/mnt/volume_lon1_01/era5_data"
+
+# Latin America
+Extent_ext <- extent(c(-118.47,-34.1,-56.65, 33.28))
+
+# Tasks
+tasks <- data.frame(
+  var = c("total_precipitation"),
+  stat = c("sum"),
+  fix = c(TRUE)
+)
+
+# Download function
+for(i in 1:nrow(tasks)){
+  
+  for(y in years){
+    
+    for(m in months){
+      
+      date_start <- as.character(floor_date(as.Date(paste0(y,"-",m,"-01")), "month"))
+      date_end <- as.character(ceiling_date(as.Date(paste0(y,"-",m,"-01")), "month")-1)
+      
+      file_name <- paste0(tasks[i,1],"_",date_start,"_",date_end,"_day_",tasks[i,2])
+      
+      message(file_name)
+      
+      if(file.exists(paste0(Dir.Data, "/", file_name, ".nc"))){
+        message("File already exists. Going for next.")
+        next
+      }
+      
+      tic()
+      QS_Raw <- download_ERA(
+        Variable = tasks[i,1],
+        PrecipFix = tasks[i,3],
+        DataSet = "era5-land",
+        DateStart = date_start,
+        DateStop = date_end,
+        TResolution = "day",
+        TStep = 1,
+        FUN = tasks[i,2],
+        Extent = Extent_ext,
+        Dir = Dir.Data,
+        FileName = file_name,
+        Cores = 1,
+        API_User = API_User,
+        API_Key = API_Key,
+        TryDown = 100
+      )
+      rm(QS_Raw)
+      unlink(paste0(normalizePath(tempdir()), "/", dir(tempdir())), recursive = TRUE)
+      toc()
+      
+    }
+    
+  }
+  
+}
